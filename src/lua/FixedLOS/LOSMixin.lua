75ab0cd5dd3372a70625e21b948bdd0027943177
Las <las@protonmail.ch>
2017-11-24 15:37:47 +0100

    Make entities that are close to enemy players visible to enemy commander

--- src/lua/FixedLOS/LOSMixin.lua
+++ src/lua/FixedLOS/LOSMixin.lua
@@ -1,3 +1,5 @@
+Script.Load "lua/Globals.lua"
+
 LOSMixin = {
 	type = "LOS",
 	expectedMixins = {
@@ -14,35 +16,94 @@ LOSMixin = {
 local kLOSTimeout                = 2
 local kLOSMaxDistanceSquared     = 7^2
 local kLOSCheckInterval          = 0.2
+local kCommanderLOSCheckInterval = 0.5
+local kLOSStructureRange         = 4
+
+local kNotRelevantToTeam1Commander = bit.bnot(kRelevantToTeam1Commander)
+local kNotRelevantToTeam2Commander = bit.bnot(kRelevantToTeam2Commander)
 
 function LOSMixin:GetIsSighted()
 	return self.sighted
 end
 
 if Server then
-	local function UpdateLOS(self)
-		local mask = bit.bor(kRelevantToTeam1Unit, kRelevantToTeam2Unit, kRelevantToReadyRoom)
-
+	local function LateInit(self)
 		local team = self:GetTeamNumber()
-		if self.sighted then
-			mask = bit.bor(mask, kRelevantToTeam1Commander, kRelevantToTeam2Commander)
-		elseif team == 1 then
-			mask = bit.bor(mask, kRelevantToTeam1Commander)
-		elseif team == 2 then
-			mask = bit.bor(mask, kRelevantToTeam2Commander)
-		end
 
-		self:SetExcludeRelevancyMask(mask)
+		self.kRelevantToEnemyCommander =
+			team == 1 and kRelevantToTeam2Commander or
+			team == 2 and kRelevantToTeam1Commander or
+			0
+		self.kNotRelevantToEnemyCommander = bit.bnot(self.kRelevantToEnemyCommander)
 
-		if self.OnSighted then
-			self:OnSighted(self.sighted)
-		end
+		local mask = self:GetExcludeRelevancyMask()
+
+		self:SetExcludeRelevancyMask(bit.bor(mask,
+			team == 1 and kRelevantToTeam1Commander or
+			team == 2 and kRelevantToTeam2Commander or
+			0
+		))
 	end
+
+	local function EnemyNear(self)
+		return #GetEntitiesForTeamWithinRange("Player", GetEnemyTeamNumber(self:GetTeamNumber()), self:GetOrigin(), kLOSStructureRange) > 0
+	end
+
+	local function Sighted(self)
+		self.sighted       = true
+		self.timeSighted   = Shared.GetTime()
+		self.originSighted = self:GetOrigin()
+
+		local mask = self:GetExcludeRelevancyMask()
+		self:SetExcludeRelevancyMask(bit.bor(mask, self.kRelevantToEnemyCommander))
+
+		self:OnSighted(true)
+	end
+
+	local function NotSighted(self)
+		self.sighted = false
+
+		if not EnemyNear(self) then
+			local mask = self:GetExcludeRelevancyMask()
+			self:SetExcludeRelevancyMask(bit.band(mask, self.kNotRelevantToEnemyCommander))
+		end
+
+		self:OnSighted(false)
+	end
+
+	local function CheckIsSighted(self)
+		if self.sighted and not (self.GetIsParasited and self:GetIsParasited()) and (
+			Shared.GetTime() - self.timeSighted > kLOSTimeout or
+			(self:GetOrigin() - self.originSighted):GetLengthSquared() > kLOSMaxDistanceSquared
+		) then
+			NotSighted(self)
+		end
+
+		return true
+	end
+
+	local function CheckIsVisibleToCommander(self)
+		if self.sighted then return true end
+
+		if not (self.GetIsCloaked and self:GetIsCloaked()) and EnemyNear(self) then
+			local mask = self:GetExcludeRelevancyMask()
+			self:SetExcludeRelevancyMask(bit.bor(mask, self.kRelevantToEnemyCommander))
+		else
+			local mask = self:GetExcludeRelevancyMask()
+			self:SetExcludeRelevancyMask(bit.band(mask, self.kNotRelevantToEnemyCommander))
+		end
+
+		return true
+	end
+
 
 	function LOSMixin:__initmixin()
 		self.sighted       = false
-		self.timeSighted   = 0
+		self.timeSighted   = -1000
 		self.originSighted = Vector()
+
+		self.kRelevantToEnemyCommander    = 0
+		self.kNotRelevantToEnemyCommander = bit.bnot(0)
 
 		self:SetExcludeRelevancyMask(bit.bor(
 			kRelevantToTeam1Unit,
@@ -50,8 +111,14 @@ if Server then
 			kRelevantToReadyRoom
 		))
 
-		self:AddTimedCallback(UpdateLOS, 0)
-		self:AddTimedCallback(self.CheckIsSighted, kLOSCheckInterval)
+		self:OnSighted(false)
+
+		self:AddTimedCallback(LateInit, 0)
+		self:AddTimedCallback(CheckIsSighted,            kLOSCheckInterval)
+		self:AddTimedCallback(CheckIsVisibleToCommander, kCommanderLOSCheckInterval)
+	end
+
+	function LOSMixin:OnSighted(sighted)
 	end
 
 	function LOSMixin:OnDamageDone(_, target)
@@ -59,41 +126,36 @@ if Server then
 		if target.GetIsAlive and not target:GetIsAlive() then return end
 		if target:GetTeamNumber() == self:GetTeamNumber() then return end
 
-		target:SetIsSighted(true)
+		if not target.sighted then
+			Sighted(target)
+		end
 	end
 
 	function LOSMixin:SetIsSighted(sighted)
 		local old = self.sighted
 
-		self.sighted       = sighted
-		self.timeSighted   = Shared.GetTime()
-		self.originSighted = self:GetOrigin()
-
 		if old ~= sighted then
-			UpdateLOS(self)
+			if sighted then
+				Sighted(self)
+			else
+				NotSighted(self)
+			end
 		end
 	end
 
-	function LOSMixin:CheckIsSighted()
-		if self.sighted and not (self.GetIsParasited and self:GetIsParasited()) and (
-			Shared.GetTime() - self.timeSighted > kLOSTimeout or
-			(self:GetOrigin() - self.originSighted):GetLengthSquared() > kLOSMaxDistanceSquared
-		) then
-			self.sighted = false
-			UpdateLOS(self)
+	function LOSMixin:OnTeamChange()
+		if self.sighted then
+			NotSighted(self)
 		end
-
-		return true
+		LateInit(self)
 	end
 
 	function LOSMixin:OnKill()
 		if self.sighted then
-			self.sighted = false
-			UpdateLOS(self)
+			NotSighted(self)
 		end
 	end
 
-	LOSMixin.OnTeamChange         = LOSMixin.OnKill
 	LOSMixin.OnUseGorgeTunnel     = LOSMixin.OnKill
 	LOSMixin.OnPhaseGateEntry     = LOSMixin.OnKill
 	LOSMixin.TriggerBeaconEffects = LOSMixin.OnKill
