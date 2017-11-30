738274657fcd9078374ca307184b37346ab28aa3
Las <las@protonmail.ch>
2017-11-30 16:39:48 +0100

    Make drifters scan around them

---
+++ src/lua/FixedLOS/Drifter.lua
@@ -0,0 +1,24 @@
+if not Server then return end
+
+local kInterval = 0.5
+local kRadius   = 8
+local function Check(self)
+	local entities = Shared.GetEntitiesWithTagInRange("LOS", self:GetOrigin(), kRadius)
+	local time = Shared.GetTime()
+
+	for i = 1, #entities do
+		local ent = entities[i]
+		if ent:GetTeamNumber() ~= self:GetTeamNumber() then
+			ent:SetIsSighted(true)
+		end
+	end
+
+	return true
+end
+
+local old = Drifter.OnCreate
+function Drifter:OnCreate()
+	old(self)
+
+	self:AddTimedCallback(Check, kInterval)
+end
--- src/lua/FixedLOS/FileHooks.lua
+++ src/lua/FixedLOS/FileHooks.lua
@@ -1,2 +1,3 @@
 ModLoader.SetupFileHook("lua/LOSMixin.lua",  "lua/FixedLOS/LOSMixin.lua",  "replace")
+ModLoader.SetupFileHook("lua/Drifter.lua",   "lua/FixedLOS/Drifter.lua",   "post")
 ModLoader.SetupFileHook("lua/TeamMixin.lua", "lua/FixedLOS/TeamMixin.lua", "post")
